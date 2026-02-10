b693e98d60c7e97ec3b54c5a933e3e216fd2370e
lostmyself8 <1056552046@qq.com>
2026-02-10 22:05:33 +0800

    Preparing to add a new machine

---
+++ src/main/java/com/jerry/meklg/common/tile/generator/TileEntityLargeWindGenerator.java
@@ -0,0 +1,4 @@
+package com.jerry.meklg.common.tile.generator;
+
+// TODO:1.4.0
+public class TileEntityLargeWindGenerator {}
---
+++ src/main/java/com/jerry/mekmm/common/tile/machine/TileEntityMultiphaseAlloyer.java
@@ -0,0 +1,4 @@
+package com.jerry.mekmm.common.tile.machine;
+
+// TODO:1.4.0
+public class TileEntityMultiphaseAlloyer {}
