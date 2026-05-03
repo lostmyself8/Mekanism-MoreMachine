d8c815ae3be3baffb28b983facac2ca308d82353
Jerry <1056552046@qq.com>
2026-05-04 01:55:53 +0800

    fix

--- src/main/java/com/jerry/meklg/common/registries/LargeGeneratorBlockTypes.java
+++ src/main/java/com/jerry/meklg/common/registries/LargeGeneratorBlockTypes.java
@@ -59,7 +59,7 @@ public class LargeGeneratorBlockTypes {
             .withGui(() -> LargeGeneratorContainerTypes.LARGE_WIND_GENERATOR)
             .withEnergyConfig(MekanismGeneratorsConfig.storageConfig.windGenerator)
             .withCustomShape(LargeGeneratorBlockShapes.LARGE_WIND_GENERATOR)
-            .with(AttributeCustomSelectionBox.JSON)
+            .with(AttributeCustomSelectionBox.JAVA)
             .withSound(GeneratorsSounds.WIND_GENERATOR)
             .with(AttributeUpgradeSupport.MUFFLING_ONLY)
             .with(MoreMachineBounding.LARGE_WIND_GENERATOR)
