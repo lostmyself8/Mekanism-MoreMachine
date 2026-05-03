158020545e0496d646bdd8779c8e14b434636471
QiuYe <qiuye2024github@163.com>
2026-05-03 22:14:36 +0800

    spotless

--- src/main/java/com/jerry/mekmm/mixin/PersistingMekanismDatagenCache.java
+++ src/main/java/com/jerry/mekmm/mixin/PersistingMekanismDatagenCache.java
@@ -1,11 +1,13 @@
 package com.jerry.mekmm.mixin;
 
 import com.jerry.datagen.PersistingDisabledProvidersProvider;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+
 import net.minecraft.WorldVersion;
 import net.minecraft.data.DataGenerator.Cached;
 import net.minecraft.data.HashCache;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -14,8 +16,9 @@ import java.util.Collection;
 
 @Mixin(Cached.class)
 public abstract class PersistingMekanismDatagenCache {
+
     @WrapOperation(method = "run", at = @At(value = "NEW", target = "net/minecraft/data/HashCache"))
-    public HashCache newHashCache(Path rootDir, Collection<String> providerIds, WorldVersion version, Operation<HashCache> original){
+    public HashCache newHashCache(Path rootDir, Collection<String> providerIds, WorldVersion version, Operation<HashCache> original) {
         HashCache constructed = original.call(rootDir, providerIds, version);
         PersistingDisabledProvidersProvider.captureGlobalCache(constructed);
         return constructed;
