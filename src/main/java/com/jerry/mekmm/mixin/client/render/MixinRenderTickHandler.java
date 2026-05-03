17c064e534b0c134fa121208fb17e89e1cf78af8
Jerry <1056552046@qq.com>
2026-05-03 16:36:56 +0800

    fix

--- src/main/java/com/jerry/mekmm/mixin/client/render/MixinRenderTickHandler.java
+++ src/main/java/com/jerry/mekmm/mixin/client/render/MixinRenderTickHandler.java
@@ -17,7 +17,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
-import net.neoforged.neoforge.client.event.RenderHighlightEvent;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -30,20 +29,20 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 @Mixin(value = RenderTickHandler.class, remap = false)
 public class MixinRenderTickHandler {
 
-    @Unique
-    private BlockEntity mekmm$tile;
-
-    @Inject(method = "onBlockHover", at = @At(value = "INVOKE", target = "Lmekanism/common/util/WorldUtils;getTileEntity(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/entity/BlockEntity;", shift = At.Shift.BY, ordinal = 1), locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void getTile(RenderHighlightEvent.Block event, CallbackInfo ci, Player player, BlockHitResult rayTraceResult, Level world, BlockPos pos, MultiBufferSource renderer, Camera info, PoseStack matrix, ProfilerFiller profiler, BlockState blockState, boolean shouldCancel, ItemStack stack, ItemConfigurator.ConfiguratorMode state, TransmissionType type) {
-        mekmm$tile = WorldUtils.getTileEntity(world, pos);
-    }
-
-    @Inject(method = "onBlockHover", at = @At(value = "INVOKE", target = "Lmekanism/common/tile/interfaces/ISideConfiguration;getConfig()Lmekanism/common/tile/component/TileComponentConfig;"), cancellable = true)
-    private void mixinOnBlockHover(RenderHighlightEvent.Block event, CallbackInfo ci) {
-        if (mekmm$tile instanceof INotNeedConfig need) {
-            if (need.notNeedConfig()) {
-                ci.cancel();
-            }
-        }
-    }
+//    @Unique
+//    private BlockEntity mekmm$tile;
+//
+//    @Inject(method = "onBlockHover", at = @At(value = "INVOKE", target = "Lmekanism/common/util/WorldUtils;getTileEntity(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/entity/BlockEntity;", shift = At.Shift.BY, ordinal = 1), locals = LocalCapture.CAPTURE_FAILSOFT)
+//    private void getTile(RenderHighlightEvent.Block event, CallbackInfo ci, Player player, BlockHitResult rayTraceResult, Level world, BlockPos pos, MultiBufferSource renderer, Camera info, PoseStack matrix, ProfilerFiller profiler, BlockState blockState, boolean shouldCancel, ItemStack stack, ItemConfigurator.ConfiguratorMode state, TransmissionType type) {
+//        mekmm$tile = WorldUtils.getTileEntity(world, pos);
+//    }
+//
+//    @Inject(method = "onBlockHover", at = @At(value = "INVOKE", target = "Lmekanism/common/tile/interfaces/ISideConfiguration;getConfig()Lmekanism/common/tile/component/TileComponentConfig;"), cancellable = true)
+//    private void mixinOnBlockHover(RenderHighlightEvent.Block event, CallbackInfo ci) {
+//        if (mekmm$tile instanceof INotNeedConfig need) {
+//            if (need.notNeedConfig()) {
+//                ci.cancel();
+//            }
+//        }
+//    }
 }
