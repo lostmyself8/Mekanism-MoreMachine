d92405726ee1eb1c3d0f765b60fd4efc4cc0fa6d
Jerry <1056552046@qq.com>
2026-05-03 16:27:43 +0800

    fix

--- src/main/java/com/jerry/mekmm/client/recipe_viewer/emi/recipe/RecyclerEmiRecipe.java
+++ src/main/java/com/jerry/mekmm/client/recipe_viewer/emi/recipe/RecyclerEmiRecipe.java
@@ -1,46 +1,46 @@
-package com.jerry.mekmm.client.recipe_viewer.emi.recipe;
-
-import com.jerry.mekmm.api.recipes.RecyclerRecipe;
-import com.jerry.mekmm.common.tile.machine.TileEntityRecycler;
-
-import mekanism.client.gui.element.GuiUpArrow;
-import mekanism.client.gui.element.bar.GuiVerticalPowerBar;
-import mekanism.client.gui.element.progress.ProgressType;
-import mekanism.client.gui.element.slot.SlotType;
-import mekanism.client.recipe_viewer.RecipeViewerUtils;
-import mekanism.client.recipe_viewer.emi.MekanismEmiRecipeCategory;
-import mekanism.client.recipe_viewer.emi.recipe.MekanismEmiHolderRecipe;
-import mekanism.common.inventory.container.slot.SlotOverlay;
-
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.RecipeHolder;
-
-import dev.emi.emi.api.stack.EmiStack;
-import dev.emi.emi.api.widget.WidgetHolder;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class RecyclerEmiRecipe extends MekanismEmiHolderRecipe<RecyclerRecipe> {
-
-    public RecyclerEmiRecipe(MekanismEmiRecipeCategory category, RecipeHolder<RecyclerRecipe> recipeHolder) {
-        super(category, recipeHolder);
-        addInputDefinition(recipe.getInput());
-        List<ItemStack> chanceOutputDefinition = recipe.getChanceOutputDefinition();
-        List<EmiStack> list = new ArrayList<>(chanceOutputDefinition.size());
-        for (ItemStack itemStack : chanceOutputDefinition) {
-            list.add(EmiStack.of(itemStack).setChance((float) recipe.getOutputChance()));
-        }
-        addOutputDefinition(list);
-    }
-
-    @Override
-    public void addWidgets(WidgetHolder widgets) {
-        addElement(widgets, new GuiUpArrow(this, 68, 38));
-        addSlot(widgets, SlotType.INPUT, 64, 17, input(0));
-        addSlot(widgets, SlotType.OUTPUT, 116, 35, output(0)).recipeContext(this);
-        addSlot(widgets, SlotType.POWER, 64, 53).with(SlotOverlay.POWER);
-        addElement(widgets, new GuiVerticalPowerBar(this, RecipeViewerUtils.FULL_BAR, 164, 16));
-        addSimpleProgress(widgets, ProgressType.BAR, 86, 38, TileEntityRecycler.BASE_TICKS_REQUIRED);
-    }
-}
+//package com.jerry.mekmm.client.recipe_viewer.emi.recipe;
+//
+//import com.jerry.mekmm.api.recipes.RecyclerRecipe;
+//import com.jerry.mekmm.common.tile.machine.TileEntityRecycler;
+//
+//import mekanism.client.gui.element.GuiUpArrow;
+//import mekanism.client.gui.element.bar.GuiVerticalPowerBar;
+//import mekanism.client.gui.element.progress.ProgressType;
+//import mekanism.client.gui.element.slot.SlotType;
+//import mekanism.client.recipe_viewer.RecipeViewerUtils;
+//import mekanism.client.recipe_viewer.emi.MekanismEmiRecipeCategory;
+//import mekanism.client.recipe_viewer.emi.recipe.MekanismEmiHolderRecipe;
+//import mekanism.common.inventory.container.slot.SlotOverlay;
+//
+//import net.minecraft.world.item.ItemStack;
+//import net.minecraft.world.item.crafting.RecipeHolder;
+//
+//import dev.emi.emi.api.stack.EmiStack;
+//import dev.emi.emi.api.widget.WidgetHolder;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class RecyclerEmiRecipe extends MekanismEmiHolderRecipe<RecyclerRecipe> {
+//
+//    public RecyclerEmiRecipe(MekanismEmiRecipeCategory category, RecipeHolder<RecyclerRecipe> recipeHolder) {
+//        super(category, recipeHolder);
+//        addInputDefinition(recipe.getInput());
+//        List<ItemStack> chanceOutputDefinition = recipe.getChanceOutputDefinition();
+//        List<EmiStack> list = new ArrayList<>(chanceOutputDefinition.size());
+//        for (ItemStack itemStack : chanceOutputDefinition) {
+//            list.add(EmiStack.of(itemStack).setChance((float) recipe.getOutputChance()));
+//        }
+//        addOutputDefinition(list);
+//    }
+//
+//    @Override
+//    public void addWidgets(WidgetHolder widgets) {
+//        addElement(widgets, new GuiUpArrow(this, 68, 38));
+//        addSlot(widgets, SlotType.INPUT, 64, 17, input(0));
+//        addSlot(widgets, SlotType.OUTPUT, 116, 35, output(0)).recipeContext(this);
+//        addSlot(widgets, SlotType.POWER, 64, 53).with(SlotOverlay.POWER);
+//        addElement(widgets, new GuiVerticalPowerBar(this, RecipeViewerUtils.FULL_BAR, 164, 16));
+//        addSimpleProgress(widgets, ProgressType.BAR, 86, 38, TileEntityRecycler.BASE_TICKS_REQUIRED);
+//    }
+//}
--- src/main/java/com/jerry/mekmm/client/recipe_viewer/emi/recipe/StamperEmiRecipe.java
+++ src/main/java/com/jerry/mekmm/client/recipe_viewer/emi/recipe/StamperEmiRecipe.java
@@ -1,39 +1,39 @@
-package com.jerry.mekmm.client.recipe_viewer.emi.recipe;
-
-import com.jerry.mekmm.api.recipes.StamperRecipe;
-import com.jerry.mekmm.common.tile.machine.TileEntityStamper;
-
-import mekanism.client.gui.element.GuiUpArrow;
-import mekanism.client.gui.element.bar.GuiVerticalPowerBar;
-import mekanism.client.gui.element.progress.ProgressType;
-import mekanism.client.gui.element.slot.SlotType;
-import mekanism.client.recipe_viewer.RecipeViewerUtils;
-import mekanism.client.recipe_viewer.emi.MekanismEmiRecipeCategory;
-import mekanism.client.recipe_viewer.emi.recipe.MekanismEmiHolderRecipe;
-import mekanism.common.inventory.container.slot.SlotOverlay;
-
-import net.minecraft.world.item.crafting.RecipeHolder;
-
-import dev.emi.emi.api.widget.WidgetHolder;
-
-// 虽然可以直接使用融合机的EmiRecipe，但复制一份应该更好
-public class StamperEmiRecipe extends MekanismEmiHolderRecipe<StamperRecipe> {
-
-    public StamperEmiRecipe(MekanismEmiRecipeCategory category, RecipeHolder<StamperRecipe> recipeHolder) {
-        super(category, recipeHolder);
-        addInputDefinition(recipe.getInput());
-        addInputDefinition(recipe.getMold());
-        addItemOutputDefinition(recipe.getOutputDefinition());
-    }
-
-    @Override
-    public void addWidgets(WidgetHolder widgetHolder) {
-        addElement(widgetHolder, new GuiUpArrow(this, 68, 38));
-        addSlot(widgetHolder, SlotType.INPUT, 64, 17, input(0));
-        addSlot(widgetHolder, SlotType.EXTRA, 64, 53, input(1));
-        addSlot(widgetHolder, SlotType.OUTPUT, 116, 35, output(0)).recipeContext(this);
-        addSlot(widgetHolder, SlotType.POWER, 39, 35).with(SlotOverlay.POWER);
-        addElement(widgetHolder, new GuiVerticalPowerBar(this, RecipeViewerUtils.FULL_BAR, 164, 15));
-        addSimpleProgress(widgetHolder, ProgressType.BAR, 86, 38, TileEntityStamper.BASE_TICKS_REQUIRED);
-    }
-}
+//package com.jerry.mekmm.client.recipe_viewer.emi.recipe;
+//
+//import com.jerry.mekmm.api.recipes.StamperRecipe;
+//import com.jerry.mekmm.common.tile.machine.TileEntityStamper;
+//
+//import mekanism.client.gui.element.GuiUpArrow;
+//import mekanism.client.gui.element.bar.GuiVerticalPowerBar;
+//import mekanism.client.gui.element.progress.ProgressType;
+//import mekanism.client.gui.element.slot.SlotType;
+//import mekanism.client.recipe_viewer.RecipeViewerUtils;
+//import mekanism.client.recipe_viewer.emi.MekanismEmiRecipeCategory;
+//import mekanism.client.recipe_viewer.emi.recipe.MekanismEmiHolderRecipe;
+//import mekanism.common.inventory.container.slot.SlotOverlay;
+//
+//import net.minecraft.world.item.crafting.RecipeHolder;
+//
+//import dev.emi.emi.api.widget.WidgetHolder;
+//
+//// 虽然可以直接使用融合机的EmiRecipe，但复制一份应该更好
+//public class StamperEmiRecipe extends MekanismEmiHolderRecipe<StamperRecipe> {
+//
+//    public StamperEmiRecipe(MekanismEmiRecipeCategory category, RecipeHolder<StamperRecipe> recipeHolder) {
+//        super(category, recipeHolder);
+//        addInputDefinition(recipe.getInput());
+//        addInputDefinition(recipe.getMold());
+//        addItemOutputDefinition(recipe.getOutputDefinition());
+//    }
+//
+//    @Override
+//    public void addWidgets(WidgetHolder widgetHolder) {
+//        addElement(widgetHolder, new GuiUpArrow(this, 68, 38));
+//        addSlot(widgetHolder, SlotType.INPUT, 64, 17, input(0));
+//        addSlot(widgetHolder, SlotType.EXTRA, 64, 53, input(1));
+//        addSlot(widgetHolder, SlotType.OUTPUT, 116, 35, output(0)).recipeContext(this);
+//        addSlot(widgetHolder, SlotType.POWER, 39, 35).with(SlotOverlay.POWER);
+//        addElement(widgetHolder, new GuiVerticalPowerBar(this, RecipeViewerUtils.FULL_BAR, 164, 15));
+//        addSimpleProgress(widgetHolder, ProgressType.BAR, 86, 38, TileEntityStamper.BASE_TICKS_REQUIRED);
+//    }
+//}
