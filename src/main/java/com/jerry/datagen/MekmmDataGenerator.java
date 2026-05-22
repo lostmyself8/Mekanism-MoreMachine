ec06d5f2afa1b5ce194f07bd8567902eb10e623a
QiuYe <qiuye2024github@163.com>
2026-05-22 20:17:19 +0800

    删除无用注释

--- src/main/java/com/jerry/datagen/MekmmDataGenerator.java
+++ src/main/java/com/jerry/datagen/MekmmDataGenerator.java
@@ -74,10 +74,8 @@ public class MekmmDataGenerator {
         DataGenerator gen = event.getGenerator();
         PackOutput output = gen.getPackOutput();
         CompletableFuture<Provider> lookupProvider = event.getLookupProvider();
-        // Client side data generators
         gen.addProvider(true, new MoreMachineLangProvider(output));
         gen.addProvider(true, new MoreMachineAssetProvider(output));
-        // Server side data generators
         gen.addProvider(true, new MoreMachineLootProvider(output, lookupProvider));
         gen.addProvider(true, new MoreMachineDataMapsProvider(output, lookupProvider));
         HashSet<String> disabledCompats = new HashSet<>();
