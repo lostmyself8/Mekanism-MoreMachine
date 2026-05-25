6dc9ca9768086d601e1274052534ded5e4992275
QiuYe <qiuye2024github@163.com>
2026-05-25 21:44:41 +0800

    spotless

--- src/main/java/com/jerry/mekmm/mixin/MixinBlockBounding.java
+++ src/main/java/com/jerry/mekmm/mixin/MixinBlockBounding.java
@@ -1,8 +1,5 @@
 package com.jerry.mekmm.mixin;
 
-import com.jerry.meklg.common.content.blocktype.LargeGeneratorBlockShapes;
-import com.jerry.meklg.common.registries.LargeGeneratorBlocks;
-
 import mekanism.common.block.BlockBounding;
 import mekanism.common.block.attribute.Attribute;
 
@@ -13,6 +10,8 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
+import com.jerry.meklg.common.content.blocktype.LargeGeneratorBlockShapes;
+import com.jerry.meklg.common.registries.LargeGeneratorBlocks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -27,7 +26,7 @@ public class MixinBlockBounding {
             "getVisualShape"
     }, at = @At("HEAD"), cancellable = true)
     private void getLargeWindGeneratorPartShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context,
-          CallbackInfoReturnable<VoxelShape> cir) {
+                                                CallbackInfoReturnable<VoxelShape> cir) {
         mekmm$setLargeWindGeneratorPartShape(world, pos, cir);
     }
 
